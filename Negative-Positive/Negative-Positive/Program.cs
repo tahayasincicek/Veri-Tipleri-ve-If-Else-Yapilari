a378f0d12b4cf32dd1f7ac9a4244470f51059e2b
Taha Yasin Çiçek <116304917+tahayasincicek@users.noreply.github.com>
2025-02-20 18:04:57 +0300

    Add files via upload

---
+++ Negative-Positive/Negative-Positive/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+
+
+Console.WriteLine("Enter a number please");
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number > 0)
+{
+    Console.WriteLine("Number is positive");
+    Console.ReadLine();
+}
+else if (number < 0)
+{
+    Console.WriteLine("Number is negative");
+    Console.ReadLine();
+}
+
+else
+{
+    Console.WriteLine("Number is zero");
+    Console.ReadLine();
+}
