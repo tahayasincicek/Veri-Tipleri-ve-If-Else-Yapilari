6e2c7a4ea54c1f8c444744dcada60098f874d3cb
Taha Yasin Çiçek <116304917+tahayasincicek@users.noreply.github.com>
2025-02-20 17:55:23 +0300

    Add files via upload

---
+++ The Biggest Number/The Biggest Number/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Enter first number");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter second number");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter third number");
+int number3 = Convert.ToInt32(Console.ReadLine());
+
+
+int biggest;
+if (number1>number2 && number1 > number3)
+{
+    biggest = number1;
+    Console.WriteLine("The biggest number is: " + biggest);
+}
+
+else if (number2 > number1 && number2 > number3)
+{
+    biggest = number2;
+    Console.WriteLine("The biggest number is: " + biggest);
+}
+else if (number3 > number1 && number3 > number1)
+{
+    biggest = number3;
+    Console.WriteLine("The biggest number is: " + biggest);
+}
